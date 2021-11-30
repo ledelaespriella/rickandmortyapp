5e51ba8633337c879ad47c0fb7211ccf86ce3489
Luis De La Espriella <luisdelaespriella@MacBook-Air-de-Luis.local>
2021-11-30 18:42:44 -0500

    se agrega el api del map

--- src/pages/maps.js
+++ src/pages/maps.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const Maps = ()=> {
+const Maps = () => {
+    const position = [51.505, -0.09];
     return (
-        <section className="bg-light">
-            <div className="container px-5">
-                <h1>View Maps</h1>
-            </div>
-        </section>
+        <>
+            <section className="bg-light">
+                <div className="container px-5">
+                    <h1>View Register</h1>
+                </div>
+            </section>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </>
     );
 };
 
